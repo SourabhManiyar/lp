679e187d092480ae63bcc197b7d9abe3e04983a8
Sourabh Maniyar <63630909+SourabhManiyar@users.noreply.github.com>
2022-05-19 01:59:27 +0530

    Add files via upload

---
+++ lp_pr 2/miniproject/init-firebase.js
@@ -0,0 +1,15 @@
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDodaez9ek03Vi7gHjoepkVj2pkGkKn-Q8",
+    authDomain: "lp2project-fe2de.firebaseapp.com",
+    projectId: "lp2project-fe2de",
+    storageBucket: "lp2project-fe2de.appspot.com",
+    messagingSenderId: "788166757506",
+    appId: "1:788166757506:web:de81eb770d807a230e389b",
+    measurementId: "G-T75B3X7G9D"
+};
+ 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
